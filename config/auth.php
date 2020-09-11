0dc08e56b3d829548e85adb29962ee6359fe0f88
Chris Thomas <chris.thomas@antimatter-studios.com>
2020-09-11 09:26:11 +0200

    support local and remote types of access control encoding

--- config/auth.php
+++ config/auth.php
@@ -1,5 +1,13 @@
 <?php
 
+$accessControl = env('ACCESS_CONTROL', '');
+
+try{
+    $users = json_decode($accessControl, true, 512, JSON_THROW_ON_ERROR);
+}catch(Exception $e){
+    $users = json_decode(base64_decode($accessControl), true);
+}
+
 return [
-    'users' => json_decode(env('ACCESS_CONTROL', '[]')),
+    'users' => $users
 ];
