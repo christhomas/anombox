65e05f573b08348957525b0645c27cd2b46ef5cf
Chris Thomas <chris.thomas@antimatter-studios.com>
2020-09-11 09:27:26 +0200

    add the administration interface endpoints to the router

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,26 @@
 |
 */
 
+session_start();
+
 $router->group(['middleware' => 'cors'], function () use ($router) {
     $router->options('{path:.*}', 'Controller@cors');
 
     $router->group(['prefix' => '/anombox/v1'], function() use ($router){
         $router->post('/upload', 'Controller@upload');
 
+        $router->get('admin', function() {
+            return view('login');
+        });
+
+        $router->get('/view', function(){
+            return view('view');
+        });
+
         $router->group(['middleware' => 'auth'], function () use ($router){
             $router->get('/list', 'Controller@list');
+            $router->delete('/delete/{filename}', 'Controller@delete');
+            $router->get('/download/{filename}', 'Controller@download');
         });
 
         $router->post('/login', 'Controller@login');
